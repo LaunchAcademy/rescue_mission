e2380a7b72252542981af73fbb48d6c914f70b9c
Eric Kelly <heroiceric@gmail.com>
2014-07-22 15:32:10 -0400

    Initial commit - backend api

---
+++ rescue_mission/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+RescueMission::Application.config.secret_token = '36e2dd26447e118d8d4717796b8828472ba654261967e7ea4c1d4aabd36242cd769e23d160566b4368cf1f788233438bcbb38546bdb029f8ddac15a084f9eed7'
