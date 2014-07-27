bbdfc027948c562fe02f27eb07bd77fcfbdca47c
Eric Kelly <heroiceric@gmail.com>
2014-07-26 23:05:19 -0400

    Add /questions to api

---
+++ backend/spec/controllers/api/v1/questions_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe API::V1::QuestionsController do
+  describe "GET #index" do
+    it "returns all the questions" do
+      older_question = FactoryGirl.create(:question, created_at: Time.zone.now - 1.week)
+      old_question = FactoryGirl.create(:question, created_at: Time.zone.now - 1.day)
+      oldest_question = FactoryGirl.create(:question, created_at: Time.zone.now - 1.year)
+      ordered_questions = [old_question, older_question, oldest_question]
+
+      get :index
+
+      expect(json).to be_json_eq(
+        ActiveModel::ArraySerializer.new(ordered_questions, root: :questions)
+      )
+    end
+  end
+end
