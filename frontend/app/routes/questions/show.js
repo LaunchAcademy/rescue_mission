8a81c825a2687f988cd231f458fbc16d39e7c3fa
Eric Kelly <heroiceric@gmail.com>
2014-07-30 14:02:21 -0400

    Reset controller state on didTransition

--- frontend/app/routes/questions/show.js
+++ frontend/app/routes/questions/show.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('question', params.id);
+  },
+
+  actions: {
+    didTransition: function() {
+      this.get('controller').set('isEditing', false);
+    }
   }
 });
