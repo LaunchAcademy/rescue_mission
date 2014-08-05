1dc2f9d370c5422c5cb3eee040e9497433c72e9a
Helen Hood <helen.c.hood@gmail.com>
2014-08-05 11:58:02 -0400

    Modify answer factory & spec

--- backend/spec/factories/answers.rb
+++ backend/spec/factories/answers.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :answer do
     association :user
     association :question
+    accepted false
 
     sequence(:body) do |n|
       "If you just derp #{n} times and refresh, it should work."
