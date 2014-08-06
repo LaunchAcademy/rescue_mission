7abf1cbde6d0cb8dc8c744d7022f7b2d495011bf
Helen Hood <helen.c.hood@gmail.com>
2014-08-06 09:30:57 -0400

    Add accept action to answer list-item controller

--- frontend/app/controllers/answers/list-item.js
+++ frontend/app/controllers/answers/list-item.js
@@ -31,6 +31,24 @@ export default Ember.ObjectController.extend(Ember.Validations.Mixin, {
       }, function() {
         _this.wuphf.danger('Something went wrong. Please try again.', 3000);
       });
+    },
+
+    accept: function() {
+      var answer = this.get('model');
+      var question = answer.get('question');
+      var _this = this;
+
+      answer.set('accepted', true);
+
+      answer.save().then(function() {
+        // THIS ISN'T PERSISTING THE CHANGE TO DB:
+        // at this point, _this.get('model').get('accepted') returns false
+        question.set('canAcceptAnswer', false);
+      }, function(){
+          _this.wuphf.danger('Something went wrong. Please try again.', 3000);
+      });
+
+      // but down here, answer.get('accepted') returns true
     }
   }
 });
