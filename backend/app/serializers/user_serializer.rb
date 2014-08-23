dc3bfab71b010e6e8936959050febdfb7a6798cf
Eric Kelly <heroiceric@gmail.com>
2014-08-23 15:44:44 -0400

    Display real user avatars from gravatar

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
   embed :ids
 
-  attributes :id, :username
+  attributes :id, :avatar_url, :username
 
   has_many :questions
+
+  def avatar_url
+    Gravatar.new(object.email).image_url
+  end
 end
--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   questions: DS.hasMany('question', { async: true }),
   answers: DS.hasMany('answer', { async: true }),
 
+  avatarUrl: DS.attr('string'),
   username: DS.attr('string')
 });
