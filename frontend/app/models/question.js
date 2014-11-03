7d904df68f9bd28d47cc68b52655378596e263ea
Eric Kelly <heroiceric@gmail.com>
2014-11-02 19:37:38 -0500

    Questions can be assigned to admins by admins

--- frontend/app/models/question.js
+++ frontend/app/models/question.js
@@ -2,10 +2,18 @@ import DS from 'ember-data';
 import Commentable from './commentable';
 
 export default Commentable.extend({
-  user: DS.belongsTo('user', { async: true }),
   answers: DS.hasMany('answer', { async: true }),
+  assignee: DS.belongsTo('user', {
+    async: true,
+    inverse: 'assignedQuestions'
+  }),
+  user: DS.belongsTo('user', {
+    async: true,
+    inverse: 'questions'
+  }),
 
   body: DS.attr('string'),
+  canAssign: DS.attr('boolean', { defaultValue: false }),
   canEdit: DS.attr('boolean', { defaultValue: false }),
   title: DS.attr('string'),
 });
