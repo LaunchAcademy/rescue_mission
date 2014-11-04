776e99e31bd6451acddae0044269f2966dc8e673
Eric Kelly <heroiceric@gmail.com>
2014-11-03 21:47:16 -0500

    Add comment for polymorphic fix

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,6 +3,7 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+// Set to false to fix polymorphic relationships
 Ember.MODEL_FACTORY_INJECTIONS = false;
 
 var App = Ember.Application.extend({
